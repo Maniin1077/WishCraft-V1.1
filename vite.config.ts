470e90789fdfe06eaf9daacc78810b607e2092d8
Maniin1077 <bsmanikanta2004@gmail.com>
2025-05-26 17:13:40 +0530

    Added public files and build output (dist), updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/WishCraft-V1.1/',  // <----- Add this line here
   server: {
     host: "::",
     port: 8080,
